86125568db6bdcf47a3fc13885977228ed7a14db
bagusindrayana <bagusindrayanaindo@gmail.com>
2024-04-21 10:54:24 +0800

    responsive dropdown menu

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -21,12 +21,12 @@
   <nav data-sveltekit-reload class="w-full flex gap-3 px-4 py-2 bg-gray-500 text-white">
     <a href="/" class="hover:underline">Home</a>
     <a href="#" class="hover:underline">About</a>
-    <div class="dropdown relative">
+    <div class="dropdown  md:relative">
       <button class="relative z-10 block" on:click={() => dropdownOpen = !dropdownOpen}>
         Wilayah
       </button>
       {#if dropdownOpen}
-        <div data-sveltekit-reload class="dropdown-menu absolute left-0 mt-2 w-56 bg-white border border-gray-200 divide-y divide-gray-100 text-black z-20">
+        <div data-sveltekit-reload class="dropdown-menu grid grid-cols-2 md:grid-cols-1 absolute left-0 mt-2 w-full md:w-60 bg-white border border-gray-200 divide-y divide-gray-100 text-black z-20">
           {#each wilayahs.sort() as wilayah (wilayah)}
             <a href="/?wilayah={wilayah}" class="block px-4 py-2 hover:bg-gray-500 hover:text-white" on:click={() => { 
                 dropdownOpen = false;
